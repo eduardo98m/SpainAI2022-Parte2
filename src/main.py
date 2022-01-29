0d228f4bcee025c615674ded0fab71d1dc2065f1
eduardo98m <eduardo98m@gmail.com>
2022-01-29 07:02:16 -0400

    added page config

--- src/main.py
+++ src/main.py
@@ -1,4 +1,30 @@
+
 import streamlit as st
 
+
+# Configuraciones de nuestra pagina web
+
+
+st.set_page_config(
+    page_title="Mira mamá mi propio modelo de ML",
+    page_icon="⚙️",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Report a bug': "https://github.com/eduardo98m/SpainAI2022-Parte2",
+    }
+)
+
+# Removal of the default Streamlit Main Menu and footer
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+
+# Funciones
 
 st.title("Hello World")
