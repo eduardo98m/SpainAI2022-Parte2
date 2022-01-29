0e7d7e714a616c4d65b1fd834535c3bd6d2585c3
eduardo98m <eduardo98m@gmail.com>
2022-01-29 07:10:44 -0400

    Sidebar pages added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import streamlit.components.v1 as components
 
 
 # Configuraciones de nuestra pagina web
@@ -24,7 +25,30 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
+#
+components.html(
+    f"""
+        <p>{st.session_state.current_page }</p>
+        <script>
+            window.parent.document.querySelector('section.main').scrollTo(0, 0);
+        </script>
+    """,
+    height=0
+)
 
+#Distintas paginas!!!#
+
+pages = ["Inicio ⚙️⚡",
+         "La Tarea 📔",
+         "El Señor Modelo 🗘",
+         ]
+
+st.session_state.current_page = st.sidebar.radio(
+    "",
+    pages)
+
+if 'current_page' not in st.session_state:
+    st.session_state.current_page = pages[0],
 # Funciones
 
-st.title("Hello World")
+st.title("Hola mama mira mi propio modelo de ML")
